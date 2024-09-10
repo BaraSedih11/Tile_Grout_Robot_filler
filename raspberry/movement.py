ebde5aaede51ee88f932866956b50a2e2c0d7cdb
Bara Al-Sedih <98843912+BaraSedih11@users.noreply.github.com>
2024-09-10 15:09:50 +0300

    sss

--- raspberry/movement.py
+++ raspberry/movement.py
@@ -2,7 +2,7 @@ import serial
 import time
 
 class Movement:
-    def __init__(self, serial_port='/dev/ttyACM0', baud_rate=9600):
+    def __init__(self, serial_port='/dev/ttyACM0', baud_rate=115200):
         self.arduino = serial.Serial(serial_port, baud_rate, timeout=1)
         self.arduino.flush()
 
