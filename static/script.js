51b23fb23a99b3d1e99a6e144e0177d088b056ba
Bara Al-Sedih <98843912+BaraSedih11@users.noreply.github.com>
2024-09-12 16:35:36 +0300

    sss

--- static/script.js
+++ static/script.js
@@ -53,7 +53,7 @@ document.getElementById("automatic").addEventListener("click", function () {
       gaps: parseFloat(gaps)
   };
 
-  fetch("http://0.0.0.0:5000/automatic-mode", {
+  fetch("/automatic-mode", {
       method: "POST",
       headers: {
           "Content-Type": "application/json"
