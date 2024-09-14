33f5a15df76d7cd4af6296691a23e69e504a0ab1
Bara Al-Sedih <98843912+BaraSedih11@users.noreply.github.com>
2024-09-14 11:11:59 +0300

    ssss

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template_string
 import cv2
 
 app = Flask(__name__)
@@ -19,6 +19,21 @@ def gen_frames():
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    # Render a simple HTML page that displays the video feed
+    return render_template_string('''
+        <html>
+            <head>
+                <title>Video Stream</title>
+            </head>
+            <body>
+                <h1>Live Video Stream</h1>
+                <img src="{{ url_for('video_feed') }}" alt="Video Feed">
+            </body>
+        </html>
+    ''')
 
 @app.route('/video_feed')
 def video_feed():
