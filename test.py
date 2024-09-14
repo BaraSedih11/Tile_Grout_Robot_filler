8f050cd8ced3e0358fdb0efe56864045c5bf8c90
Bara Al-Sedih <98843912+BaraSedih11@users.noreply.github.com>
2024-09-14 11:25:52 +0300

    ssss

--- test.py
+++ test.py
@@ -1,27 +1,9 @@
-from flask import Flask, Response
 from picamera import PiCamera
-from picamera.array import PiRGBArray
-import time
-import cv2
+from time import sleep
 
-app = Flask(__name__)
 camera = PiCamera()
-camera.resolution = (640, 480)
-raw_capture = PiRGBArray(camera, size=(640, 480))
 
-def gen_frames():
-    time.sleep(2)  # Camera warm-up time
-    for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
-        image = frame.array
-        ret, buffer = cv2.imencode('.jpg', image)
-        frame = buffer.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        raw_capture.truncate(0)
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+camera.start_preview()
+sleep(5)
+camera.capture('/home/pi/image.jpg')
+camera.stop_preview()
